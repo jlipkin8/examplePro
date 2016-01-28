1e63067a3c78baccf1d6a7c8e36eb3520cd65e5e
Josie Lipkin <jlipkin8@gmail.com>
2016-01-27 22:40:40 -0800

    Added some new colors, some JQuery effects

---
+++ examplePro.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#first').click(function(){
+		$('#first').animate({width: '200px'});
+	});
+	$('#second').click(function(){
+		$('#second').animate({width: '300px'});
+	});
+
+	$('#third').click(function(){
+		$('#third').animate({width: '400px'});
+	});
+}); 
